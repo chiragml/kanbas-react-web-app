bd80f36170d0884ca5b828746124092a54063769
Chirag Malhotra <cmalhotradelhi@gmail.com>
2024-03-05 15:33:06 -0500

    lab - redux counter reducer

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,18 +1,21 @@
-import { Link, Routes, Route } from 'react-router-dom';
-import Assignment3 from './a3';
 import Nav from '../Nav';
+import Assignment3 from './a3';
+import Assignment4 from './a4';
+import store from './store';
+import { Provider } from 'react-redux';
+import { Routes, Route, Navigate } from 'react-router';
 function Labs() {
   return (
-    <div>
-      <Nav />
-      <Link to="/Labs/a3">Assignment 3</Link>
-      <Link to="/Labs/a4">Assignment 4</Link>
-      <Routes>
-        <Route path="/a3/*" element={<Assignment3 />} />
-        <Route path="/a4" element={<Assignment3 />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Navigate to="a3" />} />
+          <Route path="a3" element={<Assignment3 />} />
+          <Route path="a4" element={<Assignment4 />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
-
 export default Labs;
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -3,23 +3,17 @@ function Nav() {
   const { pathname } = useLocation();
   return (
     <nav className="nav nav-tabs mt-2">
-      <Link
-        to="/Labs/a3"
-        className={`nav-link ${pathname.includes('a3') ? 'active' : ''}`}
-      >
+      <Link className="nav-link" to="/Labs/a3">
         A3
       </Link>
-      <Link
-        to="/Kanbas"
-        className={`nav-link ${pathname.includes('Kanbas') ? 'active' : ''}`}
-      >
-        Kanbas
+      <Link className="nav-link" to="/Labs/a4">
+        A4
       </Link>
-      <Link
-        to="/hello"
-        className={`nav-link ${pathname.includes('hello') ? 'active' : ''}`}
-      >
+      <Link className="nav-link" to="/hello">
         Hello
+      </Link>
+      <Link className="nav-link" to="/Kanbas">
+        Kanbas
       </Link>
     </nav>
   );
